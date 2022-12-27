7ed3bacb125620fc66f67cdf577d149bb51ce9d7
Andrii Moiseienko <moyseyenko.av@gmail.com>
2022-12-27 22:11:16 +0200

    3.2 - start

--- 2/2.4_REST_+_MongoDB/src/controllers/sessioncontroller.ts
+++
@@ -1,9 +0,0 @@
-import session from "express-session";
-export const FileStore = require('session-file-store')(session);
-
-export default session({
-    store: new FileStore({}),
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: true,
-})
