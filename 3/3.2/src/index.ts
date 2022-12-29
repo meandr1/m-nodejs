d7c1d93b73f4b176d1849a1729a91f9ee1d1c5a3
Andrii Moiseienko <moyseyenko.av@gmail.com>
2022-12-29 23:31:15 +0200

    3.2 - add ejs

--- 3/3.2/src/index.ts
+++ 3/3.2/src/index.ts
@@ -1,2 +1,27 @@
-import express, { Request, Response, Express } from 'express'
+import express, { Request, Response, Express } from 'express';
+import path from 'path';
+import {books} from './books'
 const app: Express = express();
+const port: number = 3000;
+
+const createPath = (fileName: string) => path.resolve(__dirname, '..', 'static', `${fileName}.ejs`)
+
+app.use(express.static(path.join(__dirname, '../static')));
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render(createPath('books-page'), { books })
+});
+
+app.get('/book/*', (req,res)=>{
+    let id: number = +req.url.substring('/book/'.length)
+
+    console.log(req.url);
+
+    res.render(createPath('book-page'))
+})
+
+app.listen(port, () => {
+    console.log(`Library's server listening on port ${port}`)
+});
